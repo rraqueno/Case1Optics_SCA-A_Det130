ee59db51d514bff31745167e92c66cc20ed01b84
Your Name <you@example.com>
2014-10-06 08:29:02 -0400

    CORRECTED RESULTS BY UPDATING link_files.bash

---
+++ pre_process.bash
@@ -0,0 +1,11 @@
+cp -uv MODIS/A2013-309-2110-LAC_SST_geoL8TOA_B11 MODIS.img
+cp -uv MODIS/A2013-309-2110-LAC_SST_geoL8TOA_B11.hdr MODIS.img.hdr
+cp -uv ImageRadLatsLons_Column_770_SCA-A_Det130_case1_309.img L8.img
+cp -uv ImageRadLatsLons_Column_770_SCA-A_Det130_case1_309.hdr L8.hdr
+cp -uv ImageRadLatsLons_Column_770_SCA-A_Det130_case1_309.bll L8.bll
+cp -uv OpticsModelPerDet11_20140527_RITedits_Y_FLIPPEDCSV.csv B11.csv
+
+echo "A" > detector.config
+echo "130" >> detector.config
+echo "descending" >> detector.config
+
